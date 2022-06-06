50ca1a3c7c4cef73a715a477e1975afd6d011d1e
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-06-06 11:55:59 -0500

    last

--- src/api/deezerApi.js
+++ src/api/deezerApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const journalApi = axios.create({
-    baseURL:'http://localhost:8080/'
+    baseURL:'https://vuejs-foxbel-music.herokuapp.com/'
+    //baseURL:'http://localhost:8080/'
 })
 export default journalApi
